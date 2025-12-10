6f4f17e94ac0c11aa3679ef41cc17056e63f5c78
ToojO <florian.d-agostino@laplateforme.io>
2025-12-10 14:49:21 +0100

    Delete Challenge_AAA-main/Local_test directory

--- Challenge_AAA-main/Local_test/static/js/index.js
+++
@@ -1,6 +0,0 @@
-function updateGauge(id, value) {
-    const fill = document.getElementById(id);
-    const deg = (value / 100) * 360;
-
-    fill.style.background = `conic-gradient(#4caf50 ${deg}deg, transparent 0deg)`;
-}
