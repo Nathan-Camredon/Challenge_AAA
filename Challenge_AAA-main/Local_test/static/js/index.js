432905dacc1ef342b486bc859933844a4db40a42
florian-d-agostino <florian.d-agostino@laplateforme.io>
2025-12-10 09:17:14 +0100

    Test Local fonctionnel

---
+++ Challenge_AAA-main/Local_test/static/js/index.js
@@ -0,0 +1,15 @@
+function updateGauge(idFill, idText, value) {
+    const angle = (value / 100) * 360;
+    document.getElementById(idFill).style.background = 
+        `conic-gradient(#4caf50 ${angle}deg, transparent 0deg)`;
+    document.getElementById(idText).innerText = value + "%";
+}
+
+// Test : valeurs aléatoires toutes les 1 sec
+setInterval(() => {
+    let cpuValue = Math.floor(Math.random() * 20);  
+    let ramValue = Math.floor(Math.random() * 40);
+
+    updateGauge("cpu-fill", "cpu-text", cpuValue);
+    updateGauge("ram-fill", "ram-text", ramValue);
+}, 1000);
